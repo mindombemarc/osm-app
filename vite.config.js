434599fcddcb45adb00f55253f730f4aad6f5d2d
Mindombe Marc <mindombemarc@gmail.com>
2025-09-04 14:06:47 +0100

    Fix: autoriser domaine Render dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0',   // permet l'accès en local sur tous les réseaux
+    port: 5173,
   },
   preview: {
     host: '0.0.0.0',
+    allowedHosts: ['osm-pfy3.onrender.com'], // ✅ autorise Render
   },
 })
